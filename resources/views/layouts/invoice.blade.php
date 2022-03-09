17c4cbb85e3cf3229dfdee0b727182c8af88156c
Ndyaakn <ndyaakn@gmail.com>
2022-03-09 17:08:26 +0700

    invoice glow up
    use a little bit make up

--- resources/views/layouts/invoice.blade.php
+++ resources/views/layouts/invoice.blade.php
@@ -1,54 +1,80 @@
 
 @extends('layouts.default')
 
-<div class="container">
-    <img class="logo" src="{{asset('img/andalas.png')}}" width="8%" alt="">
-    <div class="row">
+<div class="container" style="padding-left: 200px; padding-right: 200px;">
+<div class="card" style="padding: 10px">
+  <div class="card-body">
+  <img class="logo" src="{{asset('img/andalas.png')}}" width="15%" alt="">
+    <div class="row" style="margin-top: 20px; margin-bottom: 20px">
         <div class="col-6">
             <div class="card p-3">
-                <p style="font-size: 13px; color: #9b989f">FROM</p>
-                <p style="font-size: 18px;">Andalas LLC</p>
-                <p style="color: #9b989f">11665 Fuqua ST Suite F 610 Houston, Texas 77034</p>
-                <p style="color: #9b989f">admin@andalas.us</p>
+                <p style="font-size: 16px; color: #9b989f">FROM</p>
+                <p style="font-size: 18px; font-weight:500; margin-top: 10px;">Andalas LLC</p>
+                <p style="color: #9b989f; margin-top: 10px; font-size: 16px;">11665 Fuqua ST Suite F 610 Houston, Texas 77034</p>
+                <p style="color: #9b989f; margin-top: 10px; font-size: 16px;">admin@andalas.us</p>
                 <p style="color: #9b989f">+1 (713) 598 0728</p>
             </div>
         </div>
         <div class="col-6">
             <div class="card p-3">
-            <p style="font-size: 13px; color: #9b989f">TO</p>
-            <p style="font-size: 18px;">{{$request->name}}</p>
-            <p style="color: #9b989f">{{$request->address_product}}</p>
-            <p style="color: #9b989f">{{$request->email}}</p>
+            <p style="font-size: 16px; color: #9b989f">TO</p>
+            <p style="font-size: 18px; font-weight:500; margin-top: 10px;">{{$request->name}}</p>
+            <p style="color: #9b989f; margin-top: 10px; font-size: 16px;">{{$request->address_product}}</p>
+            <p style="color: #9b989f; margin-top: 10px; font-size: 16px;">{{$request->email}}</p>
             <p style="color: #9b989f">{{$request->no_handphone}}</p>
             </div>
         </div>
     </div>
-    <div class="row m-3">
+    <div class="row" style="margin-top: 20px; margin-bottom: 20px"> 
         <div class="col-6">
-            <p>Invoice No : <span style="color: #9b989f;">00001</span> </p>
-            <p>Invoice Date : <span style="color: #9b989f;">{{ $date }}</span></p>
+            <p style="font-size:16px">Invoice No : <span style="color: #9b989f;">00001</span> </p>
+            <p style="font-size:16px">Invoice Date : <span style="color: #9b989f;">{{ $date }}</span></p>
         </div>
         <div class="col-6">
-            <p>Due Date : <span style="color: #9b989f;">{{ $date }}</span></p>
+            <p style="font-size:16px">Due Date : <span style="color: #9b989f;">{{ $date }}</span></p>
         </div>
     </div>
     <hr>
-    <table style="border-collapse: collapse; min-width: 400px;">
+    <table class="table table-striped" style="border-collapse: collapse; min-width: 400px;">
         <thead>
             <tr>
-                <th>Item</th>
-                <th>QTY</th>
-                <th>Rate</th>
-                <th>Subtotal</th>
+                <th style="font-size:16px">Item</th>
+                <th style="font-size:16px">QTY</th>
+                <th style="font-size:16px">Rate</th>
+                <th style="font-size:16px">Subtotal</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td style="color: #9b989f">{{$request->name_product}}</td>
-                <td style="color: #9b989f">{{$request->quantity_product}}</td>
-                <td style="color: #9b989f">{{$request->product_price}}</td>
-                <td style="color: #9b989f">{{$request->total_price}}</td>
+                <td style="font-size:16px;color: #9b989f">{{$request->name_product}}</td>
+                <td style=" font-size:16px;color: #9b989f">{{$request->quantity_product}}</td>
+                <td style=" font-size:16px;color: #9b989f">{{$request->product_price}}</td>
+                <td style=" font-size:16px;color: #9b989f">{{$request->total_price}}</td>
             </tr>
         </tbody>
     </table>
+
+    <div class="row justify-content-end d-flex" style="margin-top: 45px;
+    margin-right: 2px">
+        <table class="table table-striped" style="width: 40%">
+            <thead>
+                <tr>
+                    <th style="font-size:16px; text-align: center">Invoice Summary</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="font-size:16px;color: #9b989f">Subtotal</td>
+                    <td style="font-size:16px;color: #9b989f">USD17,640.00</td>
+                </tr>
+                <tr>
+                    <td style="font-size:16px;color: #9b989f">Total</td>
+                    <td style="font-size:16px;color: #9b989f">USD17,640.00</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  </div>
+</div>
+   
 </div>
